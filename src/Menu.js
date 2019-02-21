8478def99c24d78881e2f3a99290c60caffcb6c2
nobu <ycuobu@gmail.com>
2019-02-22 00:40:21 +0900

    menu added

---
+++ src/Menu.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './Menu.css';
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={modal:""}
+    }
+
+    fileChangeHander(e)
+    {
+        e.preventDefault()
+        let reader = new FileReader()
+        let file = e.target.files[0]
+        reader.onloadend = () => {
+            let newleaf=this.props.leafdata
+            if (newleaf.imgs==null)
+            {newleaf.imgs = [reader.result]}
+            else
+            {newleaf.imgs.push([reader.result])}
+            this.props.edit(newleaf)
+        }
+        reader.readAsDataURL(file)
+    }
+
+    _switchModal(event)
+    {
+        if(this.state.modal=="")
+        {return this._getDOM(event)}
+        else
+        {return ""}
+    }
+
+    _getPositionStyle(event)
+    {
+        //let out="left:"+ event.pageX + ";top:" + event.pageY + ";"
+        let out={
+            left: event.pageX +20,
+            top: event.pageY
+        }
+        return out;
+    }
+
+
+    _getDOM(event)
+    {
+        return(
+            <div className="Menu" style={this._getPositionStyle(event)}>
+                <label  className="Menu-Command-Label"> Add Attachment
+                    <input type='file' className="Menu-Command" onChange={(e) => this.fileChangeHander(e)}></input>
+                </label>
+                <br />
+                <label  className="Menu-Command-Label"> Add Child
+                    <input type='button' className="Menu-Command" onClick={(e) => this.props.addChild(this.props.leafdata.id)}></input>
+                </label>
+                <br />
+                <label  className="Menu-Command-Label"> Add SIbling
+                    <input type='button' className="Menu-Command" onClick={(e) => this.props.addSibling(this.props.leafdata.id)}></input>
+                </label>
+            </div>
+        )
+    }
+
+    render() {
+    return (
+        <div>
+            <label  className="Menu-Command-Label"> 三
+                <input type='button' className="Menu-Command" onClick={(e) => {this.setState({modal : this._switchModal(e)}) }}></input>
+            </label>
+            {this.state.modal}
+        </div>
+    );
+    }
+}
+
+export default Menu;
