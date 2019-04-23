b0d7151c2ee779ac759966169ae4560e1862ec62
nobu <ycuobu@gmail.com>
2019-04-23 23:02:46 +0900

    1.1.4

--- src/Menu.js
+++ src/Menu.js
@@ -2,52 +2,67 @@ import React, { Component } from 'react';
 import './Menu.css';
 import Burger from './Burger'
 import MenuModal from './MenuModal'
+import onClickOutside from 'react-onclickoutside';
 
 class Menu extends Component {
 
     constructor(props) {
         super(props);
-        this.state={modal:""}
+        this.state={modalDisplay:false}
     }
 
     _closeModal() {
-        this.setState({modal:""})
+        this.setState({modalDisplay:false})
     }
 
 
-    _switchModal(event)
+    _switchModal()
     {
-        if(this.state.modal=="")
-        {return <MenuModal position={this._getPositionStyle(event)}
-                           leafdata={this.props.leafdata}
-                           edit={this.props.edit}
-                           addChild={this.props.addChild}
-                           addSibling={this.props.addSibling} />}
-        else
-        {return ""}
+        let nextState=(this.state.modalDisplay === false)
+        this.setState({modalDisplay:nextState})
     }
+    _getModal()
+    {
+        return <MenuModal position={this._getPositionStyle(event)}
+                          leafdata={this.props.leafdata}
+                          edit={this.props.edit}
+                          addChild={this.props.addChild}
+                          addSibling={this.props.addSibling}
+                          closeModal={()=>{this._closeModal()}}/>
+    }
+
 
     _getPositionStyle(event)
     {
-        let out={
+        if(this.state.modalDisplay==false)
+        {
+            const out={display:"none"}
+            return out;
+        }
+        const out={
             left: 20 ,
             top: 0
         }
         return out;
     }
 
+    handleClickOutside() {
+        this._closeModal()
+    }
+
+
     render() {
     return (
         <div className="Menu">
             <label  className="Menu-Command-Burger"><Burger/>
                 <input type='button' className="Menu-Command"
-                       onClick={(e) => {this.setState({modal : this._switchModal(e)}) }}
+                       onClick={() => {this._switchModal() }}
                 ></input>
             </label>
-            {this.state.modal}
+            {this._getModal()}
         </div>
     );
     }
 }
 
-export default Menu;
+export default onClickOutside(Menu) ;
