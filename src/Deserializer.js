8047f9825f41642150dbc7585c734101bb2aa1bd
Nobuyuki Mukai <ycuobu@gmail.com>
2019-10-19 15:13:20 +0900

    read only mode

--- src/Deserializer.js
+++ src/Deserializer.js
@@ -1,14 +1,15 @@
 import LeafData from "./LeafData";
+import Property from "./Property";
 
 export default class Deserializer
 {
     constructor(text)
     {
         this.text=text;
-        this.data=this.convertIntialData(this.text);
+        this.data=this.convertInitialData(this.text);
     }
 
-    convertIntialData(givenData)
+    convertInitialData(givenData)
     {
         if (givenData==null)
         {
@@ -19,7 +20,26 @@ export default class Deserializer
             if(typeof(givenData)=="string")
             {
                 let parsedData=JSON.parse(givenData);
-                return {root: this.convertToLeafData(parsedData.root), focusId:parsedData.focusId};
+                let root;
+                let property;
+                if("root" in parsedData)
+                {
+                    root=this.convertToLeafData(parsedData.root);
+                }
+                else
+                {
+                    root=new LeafData();
+                }
+                if("property" in parsedData)
+                {
+                    property=this.convertToProperty(parsedData.property);
+                }
+                else
+                {
+                    property=new Property();
+                }
+
+                return {root: root, property: property};
             }
             else
             {
@@ -39,4 +59,8 @@ export default class Deserializer
         return out;
     }
 
+    convertToProperty(data)
+    {
+        return Property.getNewObject(data);
+    }
 }
